f869f4b167ae675f4f0cc27e8e5d13ad21214669
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-07 13:46:16 +0200

    nix: temporary patches override

--- flake.nix
+++ flake.nix
@@ -25,6 +25,8 @@
           packages = {
             default = self'.packages.hpp-baxter;
             hpp-baxter = pkgs.python3Packages.hpp-baxter.overrideAttrs (_: {
+              # TODO: remove this after next release
+              patches = [];
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
