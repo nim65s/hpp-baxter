de5f5150894259cbf273a4b98a07cceabd7e9076
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:18:23 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,35 +1,25 @@
 {
   description = "Wrappers for Baxter robot in HPP";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-baxter = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./package.xml
-                  ./src
-                  ./srdf
-                  ./urdf
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-baxter = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./package.xml
+              ./src
+              ./srdf
+              ./urdf
+            ];
+          };
+        };
       }
     );
 }
